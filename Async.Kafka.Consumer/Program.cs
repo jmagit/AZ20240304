1aefbd479540e0e9fe1a75ca581c0b92f4790687
Javier <jmartin@grupoloyal.com>
2024-03-06 07:42:27 +0100

    eventos

---
+++ Async.Kafka.Consumer/Program.cs
@@ -0,0 +1,109 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+using System.Threading;
+
+if(args.Length < 1) {
+    Console.WriteLine("Usage: .. mode groupName");
+    return;
+}
+
+string brokerList = "localhost:9092";
+string topicName = "sensores";
+string mode = args[0].ToLower();
+string groupName = mode; // args[1];
+
+Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+var cancellationToken = cts.Token;
+
+var config = new ConsumerConfig {
+    BootstrapServers = brokerList,
+    GroupId = groupName,
+    EnableAutoOffsetStore = false,
+    EnableAutoCommit = true,
+    StatisticsIntervalMs = 5000,
+    SessionTimeoutMs = 6000,
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnablePartitionEof = true,
+    PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+};
+
+using(var consumer = new ConsumerBuilder<Ignore, string>(config)
+    // Note: All handlers are called on the main .Consume thread.
+    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+    //.SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+    //.SetPartitionsAssignedHandler((c, partitions) => {
+    //    Console.WriteLine(
+    //        "Partitions incrementally assigned: [" +
+    //        string.Join(',', partitions.Select(p => p.Partition.Value)) +
+    //        "], all: [" +
+    //        string.Join(',', c.Assignment.Concat(partitions).Select(p => p.Partition.Value)) +
+    //        "]");
+    //})
+    //.SetPartitionsRevokedHandler((c, partitions) => {
+    //    var remaining = c.Assignment.Where(atp => partitions.Where(rtp => rtp.TopicPartition == atp).Count() == 0);
+    //    Console.WriteLine(
+    //        "Partitions incrementally revoked: [" +
+    //        string.Join(',', partitions.Select(p => p.Partition.Value)) +
+    //        "], remaining: [" +
+    //        string.Join(',', remaining.Select(p => p.Partition.Value)) +
+    //        "]");
+    //})
+    //.SetPartitionsLostHandler((c, partitions) => {
+    //    Console.WriteLine($"Partitions were lost: [{string.Join(", ", partitions)}]");
+    //})
+    .Build()) {
+    consumer.Subscribe(topicName);
+
+    Action<ConsumeResult<Ignore, string>> procesa = consumeResult => {
+        var evento = JsonSerializer.Deserialize<Evento>(consumeResult.Message.Value);
+        Console.WriteLine($"Received {consumeResult.Offset}: {evento.origen} - {evento.msg} [{evento.enviado}]");
+    };
+    if(mode.StartsWith("calc")) {
+        var calc = new Dictionary<string, long>();
+        procesa = consumeResult => {
+            var evento = JsonSerializer.Deserialize<Evento>(consumeResult.Message.Value);
+            var key = evento.origen;
+            if(calc.ContainsKey(key))
+                calc[key]++;
+            else 
+                calc[key] = 1;
+            Console.WriteLine("\nResumen\n=====================");
+            foreach(var item in calc) {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            } 
+        };
+    }
+    try {
+        while(true) {
+            try {
+                var consumeResult = consumer.Consume(cancellationToken);
+
+                if(consumeResult.IsPartitionEOF) {
+                    Console.WriteLine(
+                        $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
+
+                    continue;
+                }
+                procesa(consumeResult);
+                try {
+                    consumer.StoreOffset(consumeResult);
+                } catch(KafkaException e) {
+                    Console.WriteLine($"Store Offset error: {e.Error.Reason}");
+                }
+            } catch(ConsumeException e) {
+                Console.WriteLine($"Consume error: {e.Error.Reason}");
+            }
+        }
+    } catch(OperationCanceledException) {
+        Console.WriteLine("Closing consumer.");
+        consumer.Close();
+    }
+}
+
+record Evento(string msg, string origen, DateTime enviado);
---
+++ Async.Kafka.Sensor/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+
+if(args.Length != 1) {
+    Console.WriteLine("Usage: .. sensorName");
+    return;
+}
+
+string brokerList = "localhost:9092";
+string topicName = "sensores";
+string sensorName = args[0].ToUpper();
+
+var config = new ProducerConfig { BootstrapServers = brokerList };
+
+using(var producer = new ProducerBuilder<string, string>(config).Build()) {
+    Console.WriteLine("\n-----------------------------------------------------------------------");
+    Console.WriteLine($"Producer {producer.Name} producing on topic {topicName}.");
+    Console.WriteLine("-----------------------------------------------------------------------");
+
+    for(int i = 1; i <= 100; i++) {
+        try {
+            Thread.Sleep(Random.Shared.Next(500, 1000));
+            var message =new Message<string, string> { 
+                Key = sensorName,
+                Value = $"{{\"msg\": \"Evento {i}\",\"origen\": \"{sensorName}\",\"enviado\": \"{DateTime.Now.ToString("s")}\"}}" 
+                //Value = JsonSerializer.Serialize(new Evento($"Evento {i}", sensorName, DateTime.Now))
+            };
+            var deliveryReport = await producer.ProduceAsync(topicName, message);
+            Console.WriteLine($"entrega: {deliveryReport.Offset} {message.Value}");
+        } catch(ProduceException<string, string> e) {
+            Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+        }
+    }
+}
+
+record Evento(string msg, string origen, DateTime enviado);
